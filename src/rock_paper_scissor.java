3239533586f76fd1e4c39e060128ef867698379e
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-10-06 14:41:46 -0700

    first commit

---
+++ src/rock_paper_scissor.java
@@ -0,0 +1,52 @@
+
+//Son Ngo
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class rock_paper_scissor
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Random randomizer = new Random();
+
+		//choice list
+		List<String> list = new ArrayList<String>();
+		list.add("ROCK");
+		list.add("PAPER");
+		list.add("SCISSOR");
+
+		int reply = 0;
+
+		do
+		{
+			String User = "", Comp = "";
+
+			while ((!User.equals("ROCK")) && (!User.equals("PAPER")) && (!User.equals("SCISSOR")))
+			{
+				User = JOptionPane.showInputDialog("ROCK, PAPER, SCISSOR: ");
+			}
+
+			//randomize computer's choice
+			Comp = list.get(randomizer.nextInt(list.size()));
+			System.out.printf("%s\n", Comp);
+
+			if (User.equals(Comp))
+				reply = JOptionPane.showConfirmDialog(null, "That's a tie. Do you want to continue?", "",
+						JOptionPane.YES_NO_OPTION);
+			else if (((User.equals("SCISSOR")) && (Comp.equals("PAPER")))
+					|| ((User.equals("PAPER")) && (Comp.equals("ROCK")))
+					|| ((User.equals("ROCK")) && (Comp.equals("SCISSOR"))))
+				reply = JOptionPane.showConfirmDialog(null, "You've won. Do you want to continue?", "",
+						JOptionPane.YES_NO_OPTION);
+			else
+				reply = JOptionPane.showConfirmDialog(null, "You've lost. Do you want to continue?", "",
+						JOptionPane.YES_NO_OPTION);
+		} while (reply == JOptionPane.YES_OPTION);
+	}
+}
